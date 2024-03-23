4fdbc8eccdb45d8c54d489203e4abb6301e42424
mdYeasin-cmd <yeasin200011@gmail.com>
2024-03-23 09:18:05 +0600

    User and admin creation part added

---
+++ README.md
@@ -0,0 +1,1 @@
+# ph-health-care-server
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { userRoutes } from "./app/modules/User/user.route";
+
+const app: Application = express();
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "PH Health Care server is running...",
+  });
+});
+
+app.use("/api/v1/user", userRoutes);
+
+export default app;
